d7e63b531c6fabe160d1fb97535466f7f5baa6b9
shahmimajid <im.shahmi@gmail.com>
2022-11-03 19:23:58 +0800

    Modify

--- libvirt.tf
+++ libvirt.tf
@@ -7,6 +7,14 @@ resource "libvirt_volume" "centos7-qcow2" {
   format = "qcow2"
 }
 
+resource "libvirt_volume" "kubeworker-qcow2" {
+  name = "kubeworker.qcow2"
+  pool = "libvirt" # List storage pools using virsh pool-list
+  #source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  source = "/var/lib/libvirt/boot/CentOS-7-x86_64-GenericCloud.qcow2"
+  format = "qcow2"
+}
+
 # get user data info
 data "template_file" "user_data" {
   template = "${file("${path.module}/cloud_init.cfg")}"
@@ -19,9 +27,16 @@ resource "libvirt_cloudinit_disk" "commoninit" {
   user_data      = "${data.template_file.user_data.rendered}"
 }
 
+# Use CloudInit to add the instance
+resource "libvirt_cloudinit_disk" "commoninit2" {
+  name = "commoninit2.iso"
+  pool = "libvirt" # List storage pools using virsh pool-list
+  user_data      = "${data.template_file.user_data.rendered}"
+}
+
 # Define KVM domain to create
-resource "libvirt_domain" "centos7" {
-  name   = "centos7"
+resource "libvirt_domain" "kubernetes" {
+  name   = "kubemaster01"
   memory = "1024"
   vcpu   = 1
 
@@ -48,7 +63,36 @@ resource "libvirt_domain" "centos7" {
   }
 }
 
+# Define KVM domain to create
+resource "libvirt_domain" "kubernetes" {
+  name   = "kubeworker01"
+  memory = "1024"
+  vcpu   = 1
+
+  network_interface {
+    network_name = "default" # List networks with virsh net-list
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.kubeworker-qcow2.id}"
+  }
+
+  cloudinit = "${libvirt_cloudinit_disk.commoninit2.id}"
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
 # Output Server IP
 output "ip" {
-  value = "${libvirt_domain.centos7.network_interface.0.addresses.0}"
+  value = "${libvirt_domain.kubernetes.network_interface.0.addresses.0}"
 }
